90217e5dbc4a6f65cae5359803b8686c0273a108
sol <sol.shenker@gmail.com>
2014-12-03 12:30:46 -0500

    init

---
+++ src/main/java/net/shenkers/testdependency/App.java
@@ -0,0 +1,14 @@
+package net.shenkers.testdependency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(net.shenkers.testmaven.NumberTools.getNumber());
+    }
+}
